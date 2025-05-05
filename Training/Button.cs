b53019bb51070a05fe80510fe2479c5a451680e9
monta-20 <montassarbenbrahim29@gmail.com>
2025-05-05 16:55:53 +0100

    C# for events

---
+++ Training/Button.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Training
+{
+    public class Button
+    {
+        
+        // Delegate for the button click
+        public delegate void ButtonClickHandler();
+
+        // Event triggered when button is clicked
+        public event ButtonClickHandler ButtonClicked;
+
+        // Method to simulate a button click
+        public void OnButtonClick()
+        {
+            Console.WriteLine("🔘 Button was clicked!");
+            ButtonClicked?.Invoke(); // Notify all subscribers
+        }
+
+        // Subscriber methods
+        public void ShowMessage()
+        {
+            Console.WriteLine("💬 Hello, World!");
+        }
+
+        public void LogClick()
+        {
+            Console.WriteLine("📝 Button click logged.");
+        }
+    }
+
+   
+
+}
+
--- Training/Program.cs
+++ Training/Program.cs
@@ -1,38 +1,17 @@
-﻿using System;
+﻿using Training;
 
 public class Program
 {
-    // Déclaration d'un delegate
-    public delegate double Operation(double a, double b);
-
-    // Méthodes d'opération
-    public static double Add(double x, double y) => x + y;
-    public static double Subtract(double x, double y) => x - y;
-    public static double Multiply(double x, double y) => x * y;
-    public static double Divide(double x, double y) => y != 0 ? x / y : throw new DivideByZeroException();
-
-    // Méthode qui utilise le delegate
-    public static void ExecuteOperation(double a, double b, Operation operation, string opName)
-    {
-        try
-        {
-            double result = operation(a, b);
-            Console.WriteLine($"{opName} of {a} and {b} = {result}");
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine($"{opName} failed: {ex.Message}");
-        }
-    }
-
     public static void Main()
     {
-        double num1 = 10;
-        double num2 = 5;
+        // Create a Button instance
+        Button button = new Button();
 
-        ExecuteOperation(num1, num2, Add, "Addition");
-        ExecuteOperation(num1, num2, Subtract, "Subtraction");
-        ExecuteOperation(num1, num2, Multiply, "Multiplication");
-        ExecuteOperation(num1, num2, Divide, "Division");
+        // Subscribe methods to the event
+        button.ButtonClicked += button.ShowMessage;
+        button.ButtonClicked += button.LogClick;
+
+        // Simulate button click
+        button.OnButtonClick();
     }
 }
