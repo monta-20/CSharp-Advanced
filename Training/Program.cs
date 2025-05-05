72cef5aee1ff29006b2a945c8031bdf2c1ff262f
monta-20 <montassarbenbrahim29@gmail.com>
2025-05-05 14:58:06 +0100

    extension methods

---
+++ Training/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Training;
+
+namespace Monta; 
+
+class Program
+{
+    static void Main(string[] args)
+    {   //First Use
+        int percentage = -10;
+        if (percentage.IsBetween(0, 100))
+            Console.WriteLine("Percentage is valid");
+        else
+            Console.WriteLine("Invalid percentage");
+        //Second Use
+        var user = new User
+        {
+            FirstName = "Montassar",
+            LastName = "Ben Brahim"
+        };
+
+        string fullName = user.GetFullName();
+        Console.WriteLine(fullName);
+
+    }
+}
